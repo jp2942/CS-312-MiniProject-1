b0e598b3b4e920b229bbbf744bb915267ca97c85
jp2942 <144752625+jp2942@users.noreply.github.com>
2026-09-11 13:58:51 -0700

    store blog posts in an array

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+const posts = []
 
 app.get("/", function (req, res) {
   res.render("index");
 });
 
 app.post("/posts", function (req, res) {
+    const newPost = {
+        author: req.body.author,
+        title: req.body.title,
+        content: req.body.content,
+        createdAt: new Date(),
+    };
+    posts.push(newPost);
+    console.log(posts);
     res.send("Form recieved");
 });
 
